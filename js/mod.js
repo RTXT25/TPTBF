aca33d3c2f9b91ffe51d9747580ac4013887bd07
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-04-23 16:26:36 -0400

    fix & reformat getPointGen

--- js/mod.js
+++ js/mod.js
@@ -118,38 +118,33 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	if (hasUpgrade('e', 12)) gain = gain.times(upgradeEffect('e', 12));
-		if (hasUpgrade('e', 33)) gain = gain.times(upgradeEffect('e', 33));
-	if (hasUpgrade('e', 21)) gain = gain.times(upgradeEffect('e', 21));
-		if (hasUpgrade('e', 23)) gain = gain.times(upgradeEffect('e', 23));
-			if (hasUpgrade('e', 31)) gain = gain.times(upgradeEffect('e', 31));
-	if (hasUpgrade('e', 32) && getBuyableAmount('e', 12) >= 0.1) gain = gain.times(upgradeEffect('e', 32));
-	if (hasUpgrade('q', 12)) gain = gain.times(upgradeEffect('q', 12));
-		if (hasUpgrade('q', 13)) gain = gain.times(upgradeEffect('q', 13));
-	if (hasUpgrade('q', 34)) gain = gain.times(upgradeEffect('q', 34));
-		if (hasUpgrade('q', 35)) gain = gain.times(upgradeEffect('q', 35));
-			if (hasUpgrade('q', 41)) gain = gain.times(upgradeEffect('q', 41));
-	if (hasUpgrade('h', 11)) gain = gain.times(upgradeEffect('h', 11));
-		if (hasUpgrade('h', 21)) gain = gain.times(upgradeEffect('h', 21));
-			if (hasUpgrade('h', 31)) gain = gain.times(upgradeEffect('h', 31));
-				if (hasUpgrade('h', 41)) gain = gain.times(upgradeEffect('h', 41));
-	if (getBuyableAmount('c', 11) >= 0.1 && getBuyableAmount('c', 11) < 99) gain = gain.times(5 * getBuyableAmount('c', 11) + 1);
-	if (getBuyableAmount('c', 11) >= 99) gain = gain.times(496);
-	if (getBuyableAmount('sp', 21) >= 0.1 && getBuyableAmount('sp', 12) < 9) gain = gain.times(5 ** getBuyableAmount('sp', 21));
-		if (hasUpgrade('sp', 13)) gain = gain.times(5 ** getBuyableAmount('sp', 21));
-	if (getBuyableAmount('sp', 21) >= 9) mult = mult.times(1953125);
-		if (hasUpgrade('sp', 13)) mult = mult.times(1953125);
-	if (getBuyableAmount('sp', 12) >= 0.1 && getBuyableAmount('sp', 12) < 9) gain = gain.times(((getBuyableAmount('sp', 12) * 1) + 1) ** -1);
-	if (getBuyableAmount('sp', 12) >= 9) gain = gain.times(0.1);
-	if (hasMilestone('p', 1)) gain = gain.times(player.p.divinity.add(1).pow(0.1));
-	if (hasUpgrade('e', 11)) gain = gain.times(1.5);
-	if (!hasUpgrade('ds', 24)) gain = gain.times(Math.round(100 * (player.A.achievements.length * 0.1 + 1)) / 100);
-	if (!hasUpgrade('ds', 21) && hasUpgrade('ds', 24)) gain = gain.times(Math.round(100 * (player.A.achievements.length * 0.1 + 1)) / 100);
-	if (hasUpgrade('ds', 21) && hasUpgrade('ds', 24)) gain = gain.times(Math.round(100 * (player.A.achievements.length * 0.2)) / 100);
-	if (inChallenge('ds', 11)) gain = gain.times(0.0001);
-	if (inChallenge('ds', 12)) gain = gain.times(0.000001);
-	if (inChallenge('ds', 21)) gain = gain.times(0.0000000001);
-	if (inChallenge('ds', 22)) gain = gain.times(0.0000000001);
+	if (hasUpgrade('e', 12)) gain = gain.mul(upgradeEffect('e', 12));
+		if (hasUpgrade('e', 33)) gain = gain.mul(upgradeEffect('e', 33));
+	if (hasUpgrade('e', 21)) gain = gain.mul(upgradeEffect('e', 21));
+		if (hasUpgrade('e', 23)) gain = gain.mul(upgradeEffect('e', 23));
+			if (hasUpgrade('e', 31)) gain = gain.mul(upgradeEffect('e', 31));
+	if (hasUpgrade('e', 32) && getBuyableAmount('e', 12).gt(0)) gain = gain.mul(upgradeEffect('e', 32));
+	if (hasUpgrade('q', 12)) gain = gain.mul(upgradeEffect('q', 12));
+		if (hasUpgrade('q', 13)) gain = gain.mul(upgradeEffect('q', 13));
+	if (hasUpgrade('q', 34)) gain = gain.mul(upgradeEffect('q', 34));
+		if (hasUpgrade('q', 35)) gain = gain.mul(upgradeEffect('q', 35));
+			if (hasUpgrade('q', 41)) gain = gain.mul(upgradeEffect('q', 41));
+	if (hasUpgrade('h', 11)) gain = gain.mul(upgradeEffect('h', 11));
+		if (hasUpgrade('h', 21)) gain = gain.mul(upgradeEffect('h', 21));
+			if (hasUpgrade('h', 31)) gain = gain.mul(upgradeEffect('h', 31));
+				if (hasUpgrade('h', 41)) gain = gain.mul(upgradeEffect('h', 41));
+	if (getBuyableAmount('c', 11).gt(0)) gain = gain.mul(getBuyableAmount('c', 11).mul(5).add(1));
+	if (getBuyableAmount('sp', 21).gt(0)) gain = gain.mul(5 ** getBuyableAmount('sp', 21));
+		if (hasUpgrade('sp', 13)) gain = gain.mul(5 ** getBuyableAmount('sp', 21));
+	if (getBuyableAmount('sp', 12).gt(0)) gain = gain.mul(getBuyableAmount('sp', 12).add(1).pow(-1));
+	if (hasMilestone('p', 1)) gain = gain.mul(player.p.divinity.add(1).pow(0.1));
+	if (hasUpgrade('e', 11)) gain = gain.mul(1.5);
+	if (hasUpgrade('ds', 21) && hasUpgrade('ds', 24)) gain = gain.mul(player.A.achievements.length * 0.2)
+	else gain = gain.mul(player.A.achievements.length * 0.1 + 1);
+	if (inChallenge('ds', 11)) gain = gain.mul(0.0001);
+	if (inChallenge('ds', 12)) gain = gain.mul(0.000001);
+	if (inChallenge('ds', 21)) gain = gain.mul(0.0000000001);
+	if (inChallenge('ds', 22)) gain = gain.mul(0.0000000001);
 	return gain
 }
 
