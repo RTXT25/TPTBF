eb956f9f75f7cc2f68d427278e7e11c7664408a5
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-04-27 15:52:46 -0400

    formatting

--- js/technical/canvas.js
+++ js/technical/canvas.js
@@ -10,7 +10,7 @@ function retrieveCanvasData() {
 	canvas = treeCanv;
 	ctx = canvas.getContext("2d");
 	return true;
-}
+};
 
 function resizeCanvas() {
 	if (!retrieveCanvasData()) return
@@ -19,8 +19,7 @@ function resizeCanvas() {
 	canvas.width  = window.innerWidth;
 	canvas.height = window.innerHeight;
 		drawTree();
-}
-
+};
 
 var colors_theme
 
@@ -29,30 +28,25 @@ function drawTree() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for (layer in layers){
 		if (tmp[layer].layerShown == true && tmp[layer].branches){
-			for (branch in tmp[layer].branches)
-				{
-					drawTreeBranch(layer, tmp[layer].branches[branch])
-				}
-		}
+			for (branch in tmp[layer].branches){
+				drawTreeBranch(layer, tmp[layer].branches[branch])
+			};
+		};
 		drawComponentBranches(layer, tmp[layer].upgrades, "upgrade-")
 		drawComponentBranches(layer, tmp[layer].buyables, "buyable-")
 		drawComponentBranches(layer, tmp[layer].clickables, "clickable-")
-
-	}
-}
+	};
+};
 
 function drawComponentBranches(layer, data, prefix) {
 	for(id in data) {
 		if (data[id].branches) {
-			for (branch in data[id].branches)
-			{
+			for (branch in data[id].branches){
 				drawTreeBranch(id, data[id].branches[branch], prefix + layer + "-")
-			}
-
-		}
-	}
-
-}
+			};
+		};
+	};
+};
 
 function drawTreeBranch(num1, data, prefix) { // taken from Antimatter Dimensions & adjusted slightly
 	let num2 = data
@@ -62,17 +56,15 @@ function drawTreeBranch(num1, data, prefix) { // taken from Antimatter Dimension
 		num2 = data[0]
 		color_id = data[1]
 		width = data[2] || width
-	}
-
+	};
 	if(typeof(color_id) == "number")
-		color_id = colors_theme[color_id]
+		color_id = colors_theme[color_id];
 	if (prefix) {
-		num1 = prefix + num1
-		num2 = prefix + num2
-	}
+		num1 = prefix + num1;
+		num2 = prefix + num2;
+	};
 	if (document.getElementById(num1) == null || document.getElementById(num2) == null)
-		return
-
+		return;
 	let start = document.getElementById(num1).getBoundingClientRect();
     let end = document.getElementById(num2).getBoundingClientRect();
     let x1 = start.left + (start.width / 2) + document.body.scrollLeft;
@@ -81,8 +73,8 @@ function drawTreeBranch(num1, data, prefix) { // taken from Antimatter Dimension
     let y2 = end.top + (end.height / 2) + document.body.scrollTop;
     ctx.lineWidth = width;
     ctx.beginPath();
-    ctx.strokeStyle = color_id
+    ctx.strokeStyle = color_id;
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.stroke();
-}
+};
