982e9d8dd4066def4ce7d9a59cbf12d954f761ec
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-05-07 21:40:38 -0400

    simplify code & remove oldStyle

--- js/utils/options.js
+++ js/utils/options.js
@@ -9,37 +9,25 @@ function getStartOptions() { return {
 	hideChallenges: false,
 	showStory: true,
 	forceOneTab: false,
-	oldStyle: false,
 	tooltipForcing: true,
 	colorDisplayMode: 0,
 	colorDisplay: 0,
 }};
 
 function toggleOpt(name) {
-	if (name == 'oldStyle' && styleCooldown > 0)
-		return;
 	options[name] = !options[name];
 	if (name == 'hqTree')
 		changeTreeQuality();
-	if (name == 'oldStyle')
-		updateStyle();
 };
 
 var styleCooldown = 0;
 
-function updateStyle() {
-	styleCooldown = 1;
-	let css = document.getElementById("styleStuff");
-	css.href = options.oldStyle ? "oldStyle.css" : "style.css";
-	needCanvasUpdate = true;
-};
-
 function changeTreeQuality() {
 	var on = options.hqTree;
 	document.body.style.setProperty('--hqProperty1',on?'2px solid':'4px solid');
-	document.body.style.setProperty('--hqProperty2a',on?'-4px -4px 4px rgba(0, 0, 0, 0.25) inset':'-4px -4px 4px rgba(0, 0, 0, 0) inset');
+	document.body.style.setProperty('--hqProperty2a',on?'-4px -4px 4px #00000040 inset':'-4px -4px 4px #00000000 inset');
 	document.body.style.setProperty('--hqProperty2b',on?'0px 0px 20px var(--background)':'');
-	document.body.style.setProperty('--hqProperty3',on?'2px 2px 4px rgba(0, 0, 0, 0.25)':'none');
+	document.body.style.setProperty('--hqProperty3',on?'2px 2px 4px #00000040':'none');
 };
 
 function toggleAuto(toggle) {
