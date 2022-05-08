b5694a761ccbb2d6f791029dba738d998ff5b0a9
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-05-07 21:14:03 -0400

    more system compents converted

--- js/technical/systemComponents.js
+++ js/technical/systemComponents.js
@@ -1,15 +1,7 @@
 var systemComponents = {
 	'tab-buttons': {
 		props: ['layer', 'data', 'name'],
-		template: `
-			<div class="upgRow">
-				<div v-for="tab in Object.keys(data)">
-					<button v-if="data[tab].unlocked == undefined || data[tab].unlocked" v-bind:class="{tabButton: true, notify: subtabShouldNotify(layer, name, tab), resetNotify: subtabResetNotify(layer, name, tab)}"
-					v-bind:style="[{'border-color': tmp[layer].color}, (subtabShouldNotify(layer, name, tab) ? {'box-shadow': 'var(--hqProperty2a), 0 0 20px '  + (data[tab].glowColor || defaultGlow)} : {}), tmp[layer].componentStyles['tab-button'], data[tab].buttonStyle]"
-						v-on:click="function(){player.subtabs[layer][name] = tab; updateTabFormats(); needCanvasUpdate = true;}">{{tab}}</button>
-				</div>
-			</div>
-		`
+		template: tab_buttons,
 	},
 
 	'tree-node': {
@@ -34,47 +26,23 @@ var systemComponents = {
         template: options_tab,
     },
 
-    'back-button': {
-        template: `
-        <button v-bind:class="back" onclick="goBack()">←</button>
-        `
-    },
-
 	'tooltip' : {
 		props: ['text'],
-		template: `
-		<div class="tooltip" v-html="text"></div>
-		`
+		template: '<div class="tooltip" v-html="text">',
 	},
 
 	'node-mark': {
 		props: {'layer': {}, data: {}, offset: {default: 0}, scale: {default: 1}},
-		template: `<div v-if='data'>
-			<div v-if='data === true' class='star' v-bind:style='{position: "absolute", left: (offset-10) + "px", top: (offset-10) + "px", transform: "scale( " + scale||1 + ", " + scale||1 + ")"}'></div>
-			<img v-else class='mark' v-bind:style='{position: "absolute", left: (offset-22) + "px", top: (offset-15) + "px", transform: "scale( " + scale||1 + ", " + scale||1 + ")"}' v-bind:src="data"></div>
-		</div>
-		`
+		template: node_mark,
 	},
 
 	'particle': {
 		props: ['data', 'index'],
-		template: `
-		<div><div class='particle instant' v-bind:style="[constructParticleStyle(data), data.style]" 
-			v-on:click="run(data.onClick, data)"  v-on:mouseenter="run(data.onMouseOver, data)" v-on:mouseleave="run(data.onMouseLeave, data)" ><span v-html="data.text"></span>
-		</div>
-		<svg version="2" v-if="data.color">
-		<mask v-bind:id="'pmask' + data.id">
-        <image id="img" v-bind:href="data.image" x="0" y="0" :height="data.width" :width="data.height" />
-    	</mask>
-    	</svg>
-		</div>
-		`
+		template: particle_,
 	},
 
 	'bg': {
 		props: ['layer'],
-		template: `
-		<div class ="bg" v-bind:style="[tmp[layer].style ? tmp[layer].style : {}, (tmp[layer].tabFormat && !Array.isArray(tmp[layer].tabFormat)) ? tmp[layer].tabFormat[player.subtabs[layer].mainTabs].style : {}]"></div>
-		`
-	}
+		template: bg_,
+	},
 };
